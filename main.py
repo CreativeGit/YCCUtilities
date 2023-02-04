6020f400cfd89bf10892b7634c2c42c86d5eb6a1
Delliott <dlelliott100@outlook.com>
2023-02-04 03:34:07 +0000

    improved logging format

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from ast import literal_eval as l_e
 from json import dumps
 from typing import Union
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s (%(filename)s) - %(message)s')
 load_dotenv()
 
 
